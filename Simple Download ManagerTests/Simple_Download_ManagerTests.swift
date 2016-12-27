c1b0b1630f7e824658efa3a303cab30af0d3425a
Tom Metzger <thebutcherapps@gmail.com>
2016-12-26 23:05:53 -0600

    Initial Commit

---
+++ Simple Download ManagerTests/Simple_Download_ManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Simple_Download_ManagerTests.swift
+//  Simple Download ManagerTests
+//
+//  Created by Tom Metzger on 12/26/16.
+//  Copyright © 2016 Tom Metzger. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_Download_Manager
+
+class Simple_Download_ManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
